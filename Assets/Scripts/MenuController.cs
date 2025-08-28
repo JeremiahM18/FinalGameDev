da3806e81f178bc47ca12a8d50689bd6cd15d1be
Jeremiah <jermcdon18@gmail.com>
2025-08-28 11:30:31 -0500

    changes
    final changes

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,7 +5,7 @@ public class MenuController : MonoBehaviour
 {
   public void PlayGame()
     {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("gameplay");
     }
 
     public void MainMenu()
